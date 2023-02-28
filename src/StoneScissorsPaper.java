0b283049ddd89d832edeb8528eebf7e38504756d
arnor9 <arg82@hi.is>
2023-02-28 10:51:26 +0000

    first commit

---
+++ src/StoneScissorsPaper.java
@@ -0,0 +1,8 @@
+public class StoneScissorsPaper {
+    public static void main(String[] args) {
+        String[] moves = {"rock", "paper", "scissors"};
+        String[] results = {"lose", "draw", "win"};
+        //
+
+    }
+}
