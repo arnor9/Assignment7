12b375cf01ee8a8a433efcf52454d25c16ca4d21
Róbert Jack <robertalex00@hotmail.com>
2023-02-28 11:15:02 +0000

    loka commit

--- src/StoneScissorsPaper.java
+++ src/StoneScissorsPaper.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class StoneScissorsPaper {
     public static void main(String[] args) {
-        String[] moves = {"rock", "paper", "scissors"};
-        String[] results = {"lose", "draw", "win"};
-        //
+        String[] computerMoves = {"rock", "paper", "scissors"};
+        System.out.println("Welcome to Rock, Paper, Scissors!");
+        System.out.println("Please enter rock, paper, or scissors.");
 
+        String computerMove = computerMoves[(int) (Math.random() * 3)];
+
+        Scanner move = new Scanner(System.in);
+        String humanMove = move.nextLine();
+        System.out.println("Computer chose: " + computerMove);
+
+        if (humanMove.equals(computerMove)) {
+            System.out.println("It's a draw!");
+        } else if (humanMove.equals("rock") && computerMove.equals("scissors")) {
+            System.out.println("You win!");
+        } else if (humanMove.equals("paper") && computerMove.equals("rock")) {
+            System.out.println("You win!");
+        } else if (humanMove.equals("scissors") && computerMove.equals("paper")) {
+            System.out.println("You win!");
+        } else if (humanMove.equals("rock") && computerMove.equals("paper")) {
+            System.out.println("You lose!");
+        } else if (humanMove.equals("paper") && computerMove.equals("scissors")) {
+            System.out.println("You lose!");
+        } else if (humanMove.equals("scissors") && computerMove.equals("rock")) {
+            System.out.println("You lose!");
+
+        }
     }
 }
